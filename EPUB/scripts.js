a434caa3584110419ce6d475ce1b8d2cdc470026
Saadia Omar <saadiaomar@abubakar.towfiq>
2020-03-30 11:57:08 +0300

    index corrction

--- EPUB/scripts.js
+++ EPUB/scripts.js
@@ -1,66 +1,66 @@
 //first questions
 
 function myFunction(){
-	var s = document.getElementById('item1');
-	var item1 = s.options[s.selectedIndex].value;
+	var s = document.getElementById('question1');
+	var question1 = s.options[s.selectedIndex].value;
 
-	if (item1 == 1) {
-		document.getElementById("kat").innerHTML="0/1";
+	if (question1 == 1) {
+		document.getElementById("quiz").innerHTML="0/1";
 	}
-	else if (item1 == 2) {
-			document.getElementById("kat").innerHTML="0/1";
+	else if (question1 == 2) {
+			document.getElementById("quiz1").innerHTML="0/1";
 	}
-	else if (item1 == 3) {
-		 document.getElementById("kat1").innerHTML="1/1";
+	else if (question1 == 3) {
+		 document.getElementById("quiz1").innerHTML="1/1";
 	}
 }
 
 //second question
 function myFunction(){
-	var s = document.getElementById('item2');
-	var item2 = s.options[s.selectedIndex].value;
+	var s = document.getElementById('question2');
+	var question2 = s.options[s.selectedIndex].value;
 
-	if (item2 == 1) {
-		document.getElementById("kat").innerHTML="0/1";
+	if (question2 == 1) {
+		document.getElementById("quiz").innerHTML="0/1";
 	}
-	else if (item2 == 2) {
-			document.getElementById("kat2").innerHTML="0/1";
+	else if (question2 == 2) {
+			document.getElementById("quiz2").innerHTML="0/1";
 	}
-	else if (item2 == 3) {
-		 document.getElementById("kat2").innerHTML="1/1";
+	else if (question2 == 3) {
+		 document.getElementById("quiz2").innerHTML="1/1";
 	}
 }
 
 //third question
 function myFunction(){
-	var s = document.getElementById('item3');
-	var item3 = s.options[s.selectedIndex].value;
+	var s = document.getElementById('question3');
+	var question3 = s.options[s.selectedIndex].value;
 
-	if (item3 == 1) {
-		document.getElementById("kat3").innerHTML="0/1";
+	if (question3 == 1) {
+		document.getElementById("quiz3").innerHTML="0/1";
 	}
-	else if (item3 == 2) {
-			document.getElementById("kat3").innerHTML="0/1";
+	else if (question3 == 2) {
+			document.getElementById("quiz3").innerHTML="0/1";
 	}
-	else if (item3 == 3) {
-		 document.getElementById("kat3").innerHTML="1/1";
+	else if (question3 == 3) {
+		 document.getElementById("quiz3").innerHTML="1/1";
 	}
 }
 
 //fourth question
 
 function myFunction(){
-	var s = document.getElementById('item4');
-	var item4 = s.options[s.selectedIndex].value;
+	var s = document.getElementById('question4');
+	var question4 = s.options[s.selectedIndex].value;
 
-	if (item4 == 1) {
-		document.getElementById("kat").innerHTML="0/1";
+	if (question4 == 1) {
+		document.getElementById("quiz").innerHTML="0/1";
 	}
-	else if (item4 == 2) {
-			document.getElementById("kat").innerHTML="0/1";
+	else if (question4 == 2) {
+			document.getElementById("quiz").innerHTML="0/1";
 	}
-	else if (item4 == 3) {
-		 document.getElementById("kat4").innerHTML="1/1";
+	else if (question4 == 3) {
+		 document.getElementById("quiz4").innerHTML="1/1";
 	}
 }
 
